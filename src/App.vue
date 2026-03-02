9782c83fba3ad41252af0eccdc1a6911366acd14
Daniil Kiryakov <kiriakovdaniil717@gmail.com>
2026-03-02 11:06:10 +0700

    add: added title and description

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from "vue";
+
+const product = ref('Socks')
+const description = ref('A pair of warm, fuzzy socks')
+</script>
+
+<template>
+  <h1>
+    {{ product }}
+  </h1>
+  <p>
+    {{ description }}
+  </p>
+</template>
+
+<style scoped>
+
+</style>
